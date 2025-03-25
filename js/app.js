7b70c468f116bee5fefd29b8ee6bef6c89eb2aec
NNewton-t2 <ninewton42@tntech.edu>
2025-03-24 21:31:59 -0500

    Login page -added JS functionality and bare bones reset PW page

--- js/app.js
+++ js/app.js
@@ -3,6 +3,108 @@ const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)
 const regPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
 //end global variables
 
+// Reset Password button event listener. all validation and click events for Reset Password page must be handled inside this event
+// document.querySelector('#btnLoginButton').addEventListener('click', (event) => {
+//     fetch("components/resetPassword.html") //makes http request to get the content of the login.html file from the components directory
+//     .then(response => response.text())  //takes response from fetch request and extracts its text content
+//     .then(html => { //takes the html text and runs code in this callback function
+
+//         // Back to selection screen button
+//         document.querySelector('#btnSwapLogin').addEventListener('click', function(){
+//             document.querySelector('#frmLogin').style.display = 'none';
+//             document.querySelector('#divSelect').style.display = 'block';
+//         });
+//     })
+//     .catch(error => console.error("Error fetching Login form:", error));
+// })
+
+
+// Login button event listener. all validation and click events for Login page must be handled inside this event
+document.querySelector('#btnLoginButton').addEventListener('click', (event) => {
+    fetch("components/login.html") //makes http request to get the content of the login.html file from the components directory
+    .then(response => response.text())  //takes response from fetch request and extracts its text content
+    .then(html => { //takes the html text and runs code in this callback function
+        // Add the HTML to the page
+        document.querySelector('#divContent').innerHTML += html; //finds the element with ID "divContent" and adds the HTML from the login component to its existing content.
+        
+        // Hide index, show login
+        document.querySelector('#divSelect').style.display = 'none';
+        document.querySelector('#frmLogin').style.display = 'block';
+        
+        // **********ALL event handlers for the login form below***************
+        
+        //change title for if user selects student or instructor
+        document.querySelector('#roleSelect').addEventListener('change', function() {
+            const selectedRole = document.querySelector('#roleSelect').value;
+            const loginTitleResponsive = document.querySelector('#loginTitleResponsive');
+
+            // Show the contact info section if the role is "student"
+            if (selectedRole === 'student') {
+                loginTitleResponsive.textContent = 'Welcome Students';
+            } else {
+                loginTitleResponsive.textContent = 'Welcome Instructors';
+            }
+        });
+
+        //Click event for clicking login button 
+        document.querySelector('#btnLogin').addEventListener('click', function() {
+
+            let strEmail = document.querySelector('#txtUsername').value.trim()  //grabs what the user entered into the email box
+            let strPassword = document.querySelector('#txtPassword').value  //grabs what the user entered into the password box
+            let role = document.querySelector('#roleSelect').value  //grabs what role the user selected (student or instructor)
+            let blnError = false
+            let strMessage = '' 
+            let strIsPasswordCorrect = true
+            
+            //makes sure the email is actually an email
+            if(!regEmail.test(strEmail)){  //testing strEmail against the regular expression pattern. if the test fails, false is returned, which is negated by the !, therefore, the code inside the if statement executes
+                blnError = true;
+                strMessage += '<p>You must enter a valid email</p>'
+            }
+
+            //password validtaion. NIST compliance regex
+            if (!regPassword.test(strPassword)) {
+                strMessage += "<p>Your password must be compliant with NIST Standard</p>";
+                strMessage += "</ul>";
+            }
+
+            //make sure the password is actually user's password. Logic to be added later when able to store user passwords with user accounts
+            if(!strIsPasswordCorrect){
+                strMessage += "<p>Password Incorrect!</p>";
+            }
+
+            //if student did not select a role, return an error message saying to do so
+            if (role === "") {
+                blnError = true;
+                strMessage += '<p>Please select a valid role (Student or Instructor).</p>';
+            }
+            
+            //sweet alert  pop up message
+            if (strMessage !== "") {
+                Swal.fire({
+                    title: "Oh no, you have an error",
+                    html: strMessage,
+                    icon: "error"
+                });
+            } else {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Your form has been submitted.",
+                    icon: "success"
+                });
+            }
+        });
+
+        // Back to selection screen button
+        document.querySelector('#btnSwapLogin').addEventListener('click', function(){
+            document.querySelector('#frmLogin').style.display = 'none';
+            document.querySelector('#divSelect').style.display = 'block';
+        });
+    })
+    .catch(error => console.error("Error fetching Login form:", error));
+})
+
+
 // Register button event listener. all validation and click events for registration page must be handled inside this event
 document.querySelector('#btnRegisterButton').addEventListener('click', (event) => {
     fetch("components/registration.html") //makes http request to get the content of the registration.html file from the components directory
